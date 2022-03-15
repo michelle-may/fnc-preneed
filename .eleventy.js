c5f11767ee6d9ca71afa4ae5876170fcee014147
MBLHarrison <mitchellblharrison@gmail.com>
2022-03-15 17:49:28 +0000

    one-web-hosting init and deployed
    Absolute urls now have the url filter applied to them. This takes into account
    the newly added pathPrefix option in .eleventy.js.
    
    The fetch request to the json file currently does not work in a static build.

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,18 @@
-
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const dataDir = './_data/';
-const outputDir = './dist/';
-const fs = require('fs');
+const dataDir = "./_data/";
+const outputDir = "./dist/";
+const fs = require("fs");
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   // A useful way to reference the context we are runing eleventy in
   let env = process.env.ELEVENTY_ENV;
 
   // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'layouts/base.njk');
+  config.addLayoutAlias("default", "layouts/base.njk");
 
   // Add some utility filters
-  config.addFilter("squash", require("./src/utils/filters/squash.js") );
-  config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );  
-
+  config.addFilter("squash", require("./src/utils/filters/squash.js"));
+  config.addFilter("dateDisplay", require("./src/utils/filters/date.js"));
 
   // add support for syntax highlighting
   config.addPlugin(syntaxHighlight);
@@ -24,35 +21,35 @@ module.exports = function(config) {
   config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
 
   // compress and combine js files
-  config.addFilter("jsmin", function(code) {
+  config.addFilter("jsmin", function (code) {
     const UglifyJS = require("uglify-js");
     let minified = UglifyJS.minify(code);
-      if( minified.error ) {
-          console.log("UglifyJS error: ", minified.error);
-          return code;
-      }
-      return minified.code;
+    if (minified.error) {
+      console.log("UglifyJS error: ", minified.error);
+      return code;
+    }
+    return minified.code;
   });
 
   // config.on('afterBuild', () => {
-	// 	fs.copyFileSync(dataDir + 'plans99.json', outputDir + 'plans99.json');	
-	// });
+  // 	fs.copyFileSync(dataDir + 'plans99.json', outputDir + 'plans99.json');
+  // });
 
   // pass some assets right through
   config.addPassthroughCopy("./src/site/images");
 
   // make the seed target act like prod
-  env = (env=="seed") ? "prod" : env;
+  env = env == "seed" ? "prod" : env;
   return {
     dir: {
       input: "src/site",
       output: "dist",
-      data: `_data/${env}`
+      data: `_data/${env}`,
     },
-    templateFormats : ["njk", "md", "html", "11ty.js"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
-    passthroughFileCopy: true
+    templateFormats: ["njk", "md", "html", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    pathPrefix: "/preneed/",
   };
-
 };
